5b63dcaacc7d698a26c86dc9f36756ba2a4fdad8
niboshi <niboshi000@gmail.com>
2020-01-29 10:22:30 +0000

    Add resume test

--- tests/pytorch_extensions_tests/training_tests/triggers_tests/test_once_trigger.py
+++ tests/pytorch_extensions_tests/training_tests/triggers_tests/test_once_trigger.py
@@ -5,25 +5,26 @@ import pytorch_extensions as pte
 
 
 _parametrize = pytest.mark.parametrize(
-    'iter_per_epoch,call_on_resume',
+    'iter_per_epoch,call_on_resume,resume',
     [
         # basic
-        (5, False),
+        (5, False, 4),
         # call on resume
-        (5, True),
+        (5, True, 4),
         # unaligned epoch
-        (2.5, False),
+        (2.5, False, 3),
         # unaligned epoch, call on resume
-        (2.5, True),
+        (2.5, True, 3),
         # tiny epoch
-        (0.5, False),
+        (0.5, False, 4),
         # tiny epoch, call on resume
-        (0.5, True),
+        (0.5, True, 4),
     ])
 
 
 @_parametrize
-def test_trigger(iter_per_epoch, call_on_resume):
+def test_trigger(iter_per_epoch, call_on_resume, resume):
+    del resume  # resume is ignored
     expected = [True] + [False] * 6
     finished = [False] + [True] * 6
     manager = pte.training.ExtensionsManager({}, [], 100, [])
@@ -36,7 +37,33 @@ def test_trigger(iter_per_epoch, call_on_resume):
 
 
 @_parametrize
-def test_trigger_sparse_call(iter_per_epoch, call_on_resume):
+def test_resumed_trigger(iter_per_epoch, call_on_resume, resume):
+    expected = [True] + [False] * 6
+    finished = [False] + [True] * 6
+    if call_on_resume:
+        expected[resume] = True
+        finished[resume] = False
+    manager = pte.training.ExtensionsManager({}, [], 100, [])
+    trigger = pte.training.triggers.OnceTrigger(call_on_resume)
+    for it, (e, f) in enumerate(zip(expected[:resume], finished[:resume])):
+        with manager.run_iteration(iteration=it, epoch_size=iter_per_epoch):
+            pass
+        assert trigger.finished == f
+        assert trigger(manager) == e
+    state = trigger.state_dict()
+
+    trigger2 = pte.training.triggers.OnceTrigger(call_on_resume)
+    trigger2.load_state_dict(state)
+    for it, (e, f) in enumerate(zip(expected[resume:], finished[resume:])):
+        with manager.run_iteration(iteration=it, epoch_size=iter_per_epoch):
+            pass
+        assert trigger2.finished == f
+        assert trigger2(manager) == e
+
+
+@_parametrize
+def test_trigger_sparse_call(iter_per_epoch, call_on_resume, resume):
+    del resume  # resume is ignored
     expected = [True] + [False] * 6
     finished = [False] + [True] * 6
     for _ in range(10):
