3016437b328e0e3026305eb11943a271f25ff307
Akifumi Imanishi <imanishi@preferred.jp>
2020-02-03 07:34:33 +0000

    Add resumed interval-trigger test

--- tests/pytorch_extensions_tests/training_tests/triggers_tests/test_interval_trigger.py
+++ tests/pytorch_extensions_tests/training_tests/triggers_tests/test_interval_trigger.py
@@ -1,40 +1,56 @@
-import mock
 import pytest
 
 from pytorch_extensions import training
 from pytorch_extensions.training import triggers
 
 
-_argnames = ','.join(['iters_per_epoch', 'interval', 'expected'])
-
-
 _argvalues = [
     # iteration
-    (5, (2, 'iteration'), [False, True, False, True, False, True, False]),
+    (5, (2, 'iteration'), [False, True, False, True, False, True, False], 4),
     # basic epoch
-    (1, (3, 'epoch'), [False, False, True, False, False, True, False]),
+    (1, (3, 'epoch'), [False, False, True, False, False, True, False], 4),
     # fractional epoch
-    (2, (1.5, 'epoch'), [False, False, True, False, False, True, False]),
+    (2, (1.5, 'epoch'), [False, False, True, False, False, True, False], 4),
 ]
 
 
-@pytest.mark.parametrize(_argnames, _argvalues)
-def test_trigger(iters_per_epoch, interval, expected):
-    optimizers = {'main': mock.MagicMock()}
-    epochs = -(-len(expected) // iters_per_epoch)
+@pytest.mark.parametrize(
+    'iters_per_epoch,interval,expected,resume', _argvalues)
+def test_trigger(iters_per_epoch, interval, expected, resume):
     trainer = training.ExtensionsManager(
-        {}, optimizers, epochs,
-        iters_per_epoch=iters_per_epoch)
+        {}, [], 100, iters_per_epoch=iters_per_epoch)
     trigger = triggers.IntervalTrigger(*interval)
 
     # before the first iteration, trigger should be False
-    for iteration, e in enumerate([False] + expected):
+    for e in [False] + expected:
         with trainer.run_iteration():
             assert trigger(trainer) == e
 
 
-@pytest.mark.parametrize(_argnames, _argvalues)
-def test_str(iters_per_epoch, interval, expected):
+@pytest.mark.parametrize(
+    'iters_per_epoch,interval,expected,resume', _argvalues)
+def test_resumed_trigger(iters_per_epoch, interval, expected, resume):
+    trainer = training.ExtensionsManager(
+        {}, [], 100, iters_per_epoch=iters_per_epoch)
+    trigger = triggers.IntervalTrigger(*interval)
+
+    # before the first iteration, trigger should be False
+    for e in [False] + expected[:resume]:
+        with trainer.run_iteration():
+            assert trigger(trainer) == e
+
+    state = trigger.state_dict()
+    new_trigger = triggers.IntervalTrigger(*interval)
+    new_trigger.load_state_dict(state)
+
+    for e in expected[resume:]:
+        with trainer.run_iteration():
+            assert new_trigger(trainer) == e
+
+
+@pytest.mark.parametrize(
+    'iters_per_epoch,interval,expected,resume', _argvalues)
+def test_str(iters_per_epoch, interval, expected, resume):
     trigger = triggers.IntervalTrigger(*interval)
 
     expected = 'IntervalTrigger({}, \'{}\')'.format(*interval)
