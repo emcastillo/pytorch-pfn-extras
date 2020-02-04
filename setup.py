adf99982cd7889aaafaec12d1407970c85aff3b0
Akifumi Imanishi <imanishi@preferred.jp>
2020-02-04 09:39:35 +0000

    Rename pytorch_extensions to pytorch_pfn_extras

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ import setuptools
 
 
 setuptools.setup(
-    name='pytorch_extensions',
+    name='pytorch_pfn_extras',
     description='Torch port of several chainer extensions',
     version='0.0.0',
     install_requires=['numpy', 'torch'],
     extras_require={'test': ['pytest']},
     packages=[
-        'pytorch_extensions',
-        'pytorch_extensions.nn',
-        'pytorch_extensions.nn.modules',
-        'pytorch_extensions.training',
-        'pytorch_extensions.training.extensions',
-        'pytorch_extensions.training.triggers',
+        'pytorch_pfn_extras',
+        'pytorch_pfn_extras.nn',
+        'pytorch_pfn_extras.nn.modules',
+        'pytorch_pfn_extras.training',
+        'pytorch_pfn_extras.training.extensions',
+        'pytorch_pfn_extras.training.triggers',
     ],
 )
